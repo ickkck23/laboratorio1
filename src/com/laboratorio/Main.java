8388d83abb084b8fd232cb131b3d552294839e82
ickkck23 <santiago.sabogalvQuqvirtual.edu.co>
2025-03-31 14:56:33 -0500

    se agrega un metodo

--- src/com/laboratorio/Main.java
+++ src/com/laboratorio/Main.java
@@ -4,4 +4,10 @@ public class Main {
 public static void main(String[] args) {
 System.out.println("Bienvenidos al laboratorio de Git con Java");
 }
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Hola, mundo desde Java en Eclipse!");
+    }
+}
 }
